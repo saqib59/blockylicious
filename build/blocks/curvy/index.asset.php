d8da5614336391d82c3e60345d0b5b1df2f52638
unknown <saqibali80400@gmail.com>
2023-10-01 15:09:22 +0500

    frontend rendering

--- build/blocks/curvy/index.asset.php
+++ build/blocks/curvy/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9d27df1214b92668d4ef');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f7b7a2ea20f757822753');
---
+++ build/blocks/curvy/render.php
@@ -0,0 +1,6 @@
+<?php
+$block_wrapper_attributes = get_block_wrapper_attributes();
+    // wp_send_json( $block_wrapper_attributes);
+?>
+
+<div <?= $block_wrapper_attributes; ?>>This is a test div</div>
