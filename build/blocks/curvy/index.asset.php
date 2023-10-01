0d13c9b1b4549d1e43f553d2df5461fabeb1df14
unknown <saqibali80400@gmail.com>
2023-10-02 00:51:43 +0500

    frontend rendering completed

--- build/blocks/curvy/index.asset.php
+++ build/blocks/curvy/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f7b7a2ea20f757822753');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '21343d6d678ea881d116');
--- src/blocks/curvy/render.php
+++ src/blocks/curvy/render.php
@@ -1,6 +1,31 @@
 <?php
-$block_wrapper_attributes = get_block_wrapper_attributes();
-    // wp_send_json( $block_wrapper_attributes);
+$block_wrapper_attributes = get_block_wrapper_attributes([
+    'class' => 'alignfull'
+]);
+
+$normalPath = "M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z";
+$invertedPath = "M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z";
+
+$topTransform = "scaleX(" . ($attributes['topFlipX'] ? "-1" : "1") .") rotate(" . ($attributes['topFlipY'] ? "180deg" : "0") . ")";
+$bottomTransform = "scaleY(-1) scaleX(" . ($attributes['bottomFlipX'] ? "-1" : "1") .") rotate(" . ($attributes['bottomFlipY'] ? "180deg" : "0") . ")";
 ?>
 
-<div <?= $block_wrapper_attributes; ?>>This is a test div</div>
+<div <?= $block_wrapper_attributes; ?>>
+    <div class="curve top-curve" style="display: <?= $attributes['enableTopCurve']? "block" : "none";?>; transform: <?= $topTransform; ?>; height: <?= $attributes['topHeight']; ?>px;">
+        <svg style="height: <?= $attributes['topHeight']; ?>px;
+            width: <?= $attributes['topWidth']; ?>%;" 
+            preserveAspectRatio="none" viewBox="0 0 1200 120">
+            <path fill="<?= $attributes['topColor'] ?? "white"; ?>"
+                d=<?= $attributes['topFlipY'] ? $invertedPath : $normalPath; ?>></path>
+        </svg>
+    </div>
+        <?php echo $content; ?>
+    <div class="curve bottom-curve" style="display: <?= $attributes['enableBottomCurve']? "block" : "none";?>; transform: <?= $bottomTransform; ?>; height: <?= $attributes['bottomHeight']; ?>px;">
+        <svg style="height: <?= $attributes['bottomHeight']; ?>px;
+            width: <?= $attributes['bottomWidth']; ?>%;" 
+            preserveAspectRatio="none" viewBox="0 0 1200 120">
+            <path fill="<?= $attributes['bottomColor'] ?? "white"; ?>"
+                d=<?= $attributes['bottomFlipY'] ? $invertedPath : $normalPath; ?>></path>
+        </svg>
+    </div>
+</div>
